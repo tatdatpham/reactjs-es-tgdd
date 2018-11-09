b2d9b7ceb1219ce887205d1b190268fdaa7f2f29
Tat Dat Pham <tatdat171@gmail.com>
2018-11-09 18:58:24 +0700

    Updates

---
+++ precache-manifest.a39ad6bcc18e11f0984ecaf85ad43eda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "add5fa52e51566da48750650c9f6713e",
+    "url": "/reactjs-es-tgdd/static/media/visa.add5fa52.png"
+  },
+  {
+    "revision": "1c6ed33a78de67f80184",
+    "url": "/reactjs-es-tgdd/static/js/runtime~main.1c6ed33a.js"
+  },
+  {
+    "revision": "3c9f0c50c1dfcb199f75",
+    "url": "/reactjs-es-tgdd/static/js/main.3c9f0c50.chunk.js"
+  },
+  {
+    "revision": "e0bce4bdcb6c6e0986df",
+    "url": "/reactjs-es-tgdd/static/js/1.e0bce4bd.chunk.js"
+  },
+  {
+    "revision": "3c9f0c50c1dfcb199f75",
+    "url": "/reactjs-es-tgdd/static/css/main.e7cc6ea2.chunk.css"
+  },
+  {
+    "revision": "e0bce4bdcb6c6e0986df",
+    "url": "/reactjs-es-tgdd/static/css/1.52abfdf5.chunk.css"
+  },
+  {
+    "revision": "6f922bd62706c736a1d99e7d54a7b495",
+    "url": "/reactjs-es-tgdd/index.html"
+  }
+];
